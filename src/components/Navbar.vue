902f9db8803813f17803c4b5158880e7fc1d932a
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-08-03 19:29:49 +0200

    moved public files into the assets folder to call them easier, then updated the Navbar file to obtain the correct image source

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/">
             <button class="flex items-center space-x-3 rtl:space-x-reverse">
-              <img src="online-shop.png" class="h-8" alt="Flowbite Logo" />
+              <img src="../assets/online-shop.png" class="h-8" alt="Flowbite Logo" />
               <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
                 SwiftCart
               </span>
