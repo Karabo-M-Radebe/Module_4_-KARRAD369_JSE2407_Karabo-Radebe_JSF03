8ceddc7035757e757e5a0bb2ba8809e3e9c7777f
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-08-04 22:33:43 +0200

    added the sorting function in the Sort.vue component that targets the value of the prices

--- src/components/Sort.vue
+++ src/components/Sort.vue
@@ -1,5 +1,27 @@
-<script setup></script>
+<script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    onSortChange: { type: Function, required: true }
+  });
+  
+  const handleSortChange = (event) => {
+    props.onSortChange(event.target.value);
+  };
+</script>
 
-<template></template>
-
+<template>
+    <div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
+      <label for="sort" class="w-20 my-auto font-semibold">
+        Sort by:
+      </label>
+      <select id="sort" @change="handleSortChange" class="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+        <option value="default">Default</option>
+        <option value="lowToHigh">Price: Low to High</option>
+        <option value="highToLow">Price: High to Low</option>
+      </select>
+    </div>
+</template>
+  
+    
 <style></style>
