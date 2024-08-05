dbf112420694cfc9bb280327b5950103060caec9
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-08-05 21:10:25 +0200

    created the filter function in the 'Filter.vue' file along with it's styling

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,5 +1,33 @@
-<script setup></script>
+<script setup>
+  import { ref, watchEffect } from 'vue';
+  
+  const props = defineProps({
+    categories: { type: Array, default: () => [] },
+    selectedCategory: { type: String, default: '' },
+    onCategoryChange: { type: Function, default: () => {} }
+  });
+  
+  const selectedCategory = ref(props.selectedCategory);
+  
+  const handleChange = (event) => {
+    props.onCategoryChange(event.target.value);
+  };
+  
+  watchEffect(() => {
+    selectedCategory.value = props.selectedCategory;
+  });
+</script>
 
-<template></template>
-
-<style></style>
+<template>
+    <div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
+      <label for="category" class="w-20 my-auto font-semibold">
+        Filter:
+      </label>
+      <select id="category" @change="handleChange" v-model="selectedCategory" class="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+        <option value="">All categories</option>
+        <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
+      </select>
+    </div>
+</template>
+  
+  
